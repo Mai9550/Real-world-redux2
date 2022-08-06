187cf9ab91270ce01edd9dc92efac6a45b7a56ae
mai95 <mai_saasaa@yahoo.com>
2022-08-06 19:01:24 +0200

    12th

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
+const middleware = applyMiddleware(promiseMiddleware);
 const store = createStore(reducer,middleware)
 ReactDOM.render(
 <Provider store={store}>
